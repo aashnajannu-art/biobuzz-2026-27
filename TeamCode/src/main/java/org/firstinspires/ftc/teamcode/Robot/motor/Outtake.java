5947d8ec1110939dbc39b1fbb392ff23bb54ada8
aashnajannu-art <aashnajannu@gmail.com>
2026-09-12 19:29:40 -0700

    Adding basic files based on DECODE repo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/motor/Outtake.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Robot.motor;
+
+public class Outtake {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Main.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Main", group = "Linear Opmode")
+public class Main {
+}
