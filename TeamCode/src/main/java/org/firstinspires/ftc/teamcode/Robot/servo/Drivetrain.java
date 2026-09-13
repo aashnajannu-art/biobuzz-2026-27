732eb84d3cde0b253be684dda3181de3e4230f73
aashnajannu-art <aashnajannu@gmail.com>
2026-09-12 19:13:43 -0700

    Update PedroPathing to v3.0.0 revhub + import pedro package

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/servo/Drivetrain.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Robot.servo;
+
+public class Drivetrain {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.pedro;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Constants {
+
+    //change based on drivetrain and localizer type
+    public static Follower create(HardwareMap h) {
+        // return new Follower(Drivetrain, Localizer, Foresight);
+        return null;
+    }
+}
